58713343b523e8625ede96200a21f7beebe60603
yasanthaniroshan <niroshanyi@gmail.com>
2024-05-17 00:14:19 +0530

    Added Changes - Removed uploaded folder from tracking - Adjusting submit post handler function

--- Octa-Hub/src/routes/submit/+page.server.js
+++ Octa-Hub/src/routes/submit/+page.server.js
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 export const actions = {
 	upload: async ({ request }) => {
 		const data = Object.fromEntries(await request.formData());
-		const filePath = "src/data/"+ data.pdf_file.name;
+		const filePath = "src/files/uploaded/"+ data.pdf_file.name;
 		fs.writeFile(filePath, new Uint8Array(data.pdf_file));
 
 		return {
