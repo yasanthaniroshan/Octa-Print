e7a8f4254b93d0440922c0cca15460120aeb3fc0
yasanthaniroshan <niroshanyi@gmail.com>
2024-05-16 23:05:47 +0530

    Added basic Functionality for website - Added File Upload Functionality

--- Octa-Hub/src/routes/+page.svelte
+++ Octa-Hub/src/routes/+page.svelte
@@ -1,8 +1,3 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
 
 <svelte:head>
 	<title>Home</title>
@@ -10,22 +5,10 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h1>Welcome to the Octa-Print</h1>
+	<div class="welcome">
+		<a href="/submit" class="button">Getting Started</a>
+	</div>
 </section>
 
 <style>
@@ -42,18 +25,18 @@
 	}
 
 	.welcome {
-		display: block;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		position: relative;
 		width: 100%;
 		height: 0;
 		padding: 0 0 calc(100% * 495 / 2048) 0;
 	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 </style>
